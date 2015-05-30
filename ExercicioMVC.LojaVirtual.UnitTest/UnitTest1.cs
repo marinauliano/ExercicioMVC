5bce52c651ba044622c239cad02b683345c3b66b
marinauliano <marina.miyu@gmail.com.br>
2015-05-30 18:02:27 -0300

    Initial commit

---
+++ ExercicioMVC.LojaVirtual.UnitTest/UnitTest1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace ExercicioMVC.LojaVirtual.UnitTest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
