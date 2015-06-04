8a23d8495bac0d67761998473e4da2c24a48150f
marinauliano <marina.miyu@gmail.com.br>
2015-06-04 20:04:34 -0300

    commit produto

---
+++ ExercicioMVC.LojaVirtual.Dominio/Entidades/Produto.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExercicioMVC.LojaVirtual.Dominio.Entidades
+{
+    class Produto
+    {
+        public int ProdutoId {get; set;}
+
+        public String Type { get; set; }
+
+        public String Descricao { get; set; }
+
+        public decimal Preco { get; set; }
+
+        public String categoria { get; set; }
+
+    }
+}
